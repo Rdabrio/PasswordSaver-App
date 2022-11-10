d09e34758c87be7197d6b2ede6c9b4e566480678
dabrio <ruben.dabrio@gmail.com>
2022-11-10 22:44:01 +0100

    Password + PasswordList

---
+++ FONTS/src/domain/classes/Password.java
@@ -0,0 +1,25 @@
+package domain.classes;
+
+public class Password {
+
+    private String id;
+    private String info;
+
+    public Password(String id, String info){
+        this.id = id;
+        this.info = info;
+    }
+
+    public Password(String id) {
+        this.id = id;
+        this.info = "";
+    }
+
+    public String getId() { return this.id; }
+
+    public String getInfo() { return this.info; }
+
+    public void setInfo(String newInfo) { this.info = newInfo; }
+
+    public void setId(String newId) { this.id = newId; }
+}
---
+++ FONTS/src/domain/classes/PasswordList.java
@@ -0,0 +1,53 @@
+package domain.classes;
+
+import domain.classes.Password;
+
+import java.util.HashMap;
+
+public class PasswordList {
+
+    private HashMap<String, Password> passwords;
+
+    public PasswordList() { this.passwords = new HashMap<>(); }
+
+    public boolean existsPassword(String passwordId) {
+        if (this.passwords.size() == 0 || !this.passwords.containsKey(passwordId)) return false;
+        return true;
+    }
+
+    public boolean addPassword(String passwordId, String passwordInfo) {
+        if (existsPassword(passwordId)) return false;
+        Password p = new Password(passwordId, passwordInfo);
+        this.passwords.put(passwordId, p);
+        return true;
+    }
+
+    public boolean removePassword(String passwordId) {
+        if (!existsPassword(passwordId)) return false;
+        this.passwords.remove(passwordId);
+        return true;
+    }
+
+    public Password getPassword(String passwordId) {
+        if (!existsPassword(passwordId)) throw new RuntimeException("The password for "+" doesn't exists");
+        return this.passwords.get(passwordId);
+
+    }
+
+    public boolean modifyPassword(String currentId, String newId, String newInfo) {
+        if (!existsPassword(currentId)) return false;
+        boolean changed = false;
+        if (!newId.equals("")) {
+            Password p = this.passwords.get(currentId);
+            p.setId(newId);
+            this.passwords.remove(currentId);
+            this.passwords.put(newId, p);
+            changed = true;
+        }
+        if (!newInfo.equals("")) {
+            if (changed) this.passwords.get(newId).setInfo(newInfo);
+            else this.passwords.get(currentId).setInfo(newInfo);
+        }
+        return true;
+    }
+}
