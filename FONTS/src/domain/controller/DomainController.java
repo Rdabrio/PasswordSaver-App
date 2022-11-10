c0c1d5a859e0fe2fb943cedeac6aa15c9922fc82
dabrio <ruben.dabrio@gmail.com>
2022-11-10 22:51:33 +0100

    Password + PasswordList + domainController

---
+++ FONTS/src/domain/controller/DomainController.java
@@ -0,0 +1,10 @@
+package domain.controller;
+
+import domain.classes.*;
+
+public class DomainController {
+
+    private PasswordList passwords;
+
+    public DomainController() { this.passwords = new PasswordList(); }
+}
